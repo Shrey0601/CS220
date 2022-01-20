34cef56d084740223cff6ad0fcafec8efe635e59
nsingla20 <nsingla20@iitk.ac.in>
2022-01-21 03:33:40 +0530

    Added Q1

---
+++ assignments/1/q1/1_bit_adder.v
@@ -0,0 +1,16 @@
+// Define a l-bit full adder 
+module bitadd(sum, cout, a, b, cin) ; 
+    // I/O port declarations 
+    output sum, cout; 
+    input a, b, cin; 
+
+    // Internal nets 
+    wire s1, c1, c2; 
+
+    // Instantiate logic gate primitives 
+    xor (s1, a, b); 
+    and (c1, a, b); 
+    xor (sum, s1, cin) ; 
+    and (c2, s1, cin); 
+    or (cout, c2, c1);
+endmodule
