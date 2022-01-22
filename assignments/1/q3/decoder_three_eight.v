dbd73c0ab452e613ba7283f33d72f2cbbca0719a
Shrey Mehta <79155940+Shrey0601@users.noreply.github.com>
2022-01-22 17:55:25 +0530

    Update decoder_three_eight.v

--- assignments/1/q3/decoder_three_eight.v
+++ assignments/1/q3/decoder_three_eight.v
@@ -1,4 +1,7 @@
+// Including the 2-4 decoder module containing file
 `include "decoder_two_four.v"
+
+// Define a 3-8 decoder
 module decoder_three_eight (
     input in0,
     input in1,
@@ -13,6 +16,9 @@ module decoder_three_eight (
     output d6,
     output d7
 );
+    
+    // 3-8 decoder implemented using a 2-4 decoder
+    // The decoder uses an and gate to ensure that the functioning is done with two 2-4 decoders itself
     
    decoder_two_four a0(in0, in1, ((~in2) & en), d0, d1, d2, d3);
    decoder_two_four a1(in0, in1, (in2 & en), d4, d5, d6, d7);
