4a64b5935e5f981c6b452c2dd1fbd9fdc365914b
Shrey Mehta <79155940+Shrey0601@users.noreply.github.com>
2022-01-22 18:03:19 +0530

    Update decoder_three_eight.v

--- assignments/1/q3/decoder_three_eight.v
+++ assignments/1/q3/decoder_three_eight.v
@@ -19,6 +19,9 @@ module decoder_three_eight (
     
     // 3-8 decoder implemented using a 2-4 decoder
     // The decoder uses an and gate to ensure that the functioning is done with two 2-4 decoders itself
+    // The output of the 3-8 decoder is and( in0/(~in0), in1/(~in1), in2/(~in2))
+    // So to include the enable signal in the ouput as well using only 2 2-4 decoders, we replace any one signal, say in2/(~in2) with (in2 & en)/ ((~in2) & en)
+    // Hence this decoder does the job using only two 2-4 decoders and an and gate 
     
    decoder_two_four a0(in0, in1, ((~in2) & en), d0, d1, d2, d3);
    decoder_two_four a1(in0, in1, (in2 & en), d4, d5, d6, d7);
